3f8d82a651df5e8e4194bc1a4be749e5077245d8
Sook-Hee <sook-hee.evans@tufts.edu>
2020-05-01 14:33:38 -0400

    renders new screen after correct click

---
+++ js/setup.js
@@ -0,0 +1,10 @@
+function newSetup() {
+    $(".column").css("background-color", "lightyellow");
+    var num_entries = Math.round(Math.random() * 10);
+    num_entries = 1;
+    $("#word").html(dict.words[num_entries].word)
+    for (j in dict.words[num_entries].matches) {
+        var word = "#word" + j;
+        $(word).html(dict.words[num_entries].matches[j])
+    }
+}
