04e3c5207fca3e4a74cb3ff1695519cf7a58d522
Sook-Hee <sook-hee.evans@tufts.edu>
2020-05-01 13:20:43 -0400

    added 2 minute timer

---
+++ js/timer.js
@@ -0,0 +1,16 @@
+// Update the count down every 1 second
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+function startTimer() {
+    console.log('starting timer')
+    var timeleft = 120;
+    var downloadTimer = setInterval(function(){
+      if(timeleft <= 0){
+        clearInterval(downloadTimer);
+      }
+      document.getElementById("demo").value = 10 - timeleft;
+      document.getElementById("demo").innerHTML = timeleft;
+      timeleft -= 1;
+    }, 1000);
+}
