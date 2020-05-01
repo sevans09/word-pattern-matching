141045edabac154922d5fc967e0d8a3d4b094e1e
Sook-Hee <sook-hee.evans@tufts.edu>
2020-05-01 14:44:46 -0400

    continuously generates new words

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,6 @@
 function newSetup() {
     $(".column").css("background-color", "lightyellow");
-    var num_entries = Math.round(Math.random() * 10);
-    num_entries = 1;
+    var num_entries = Math.round(Math.random() * 10) - 1;
     $("#word").html(dict.words[num_entries].word)
     for (j in dict.words[num_entries].matches) {
         var word = "#word" + j;
