caf7c64c5db898e1456752f125034bc11e3c9379
Sook-Hee <sook-hee.evans@tufts.edu>
2020-05-13 16:41:04 -0400

    add post function created

--- js/playgame.js
+++ js/playgame.js
@@ -21,8 +21,34 @@ function outOfTime() {
   var curr_score = str.split(" ").pop();
   console.log('curr score is', curr_score);
   $("#scoreModal").text("Score: " + curr_score);
-
   $('#endModal').modal('show');
+  addPost("sook", curr_score);
 }
 
 
+function addPost(name, score) {
+    const database = firebase.database().ref('Scores');
+    const postRef = database.push();
+
+    const postData = {
+        player: name,
+        playerScore: score,
+    };
+    console.log("in add post", name, score)
+
+    postRef.set(postData)
+    .then(() => {
+      const success = {
+        success: true,
+      };
+      console.log("post added")
+      return success;
+    })
+    .catch((error) => {
+      const success = {
+        success: false,
+        errorMessage: error.message,
+      };
+      return success;
+    });
+}
