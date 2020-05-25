9542d7e56794400562fff96f21733212e1bb09f7
Sook-Hee <sook-hee.evans@tufts.edu>
2020-05-25 09:34:22 -0400

    fixed leaderboard bug

--- js/playgame.js
+++ js/playgame.js
@@ -9,6 +9,7 @@ function startGame() {
         firstName = snapshot.val().topName;
         secondName = snapshot.val().secondName;
         thirdName = snapshot.val().thirdName;
+        console.log('third nam and score', thirdName, third)
 
         leaderboard.innerText += firstName + " " + first + "\n";
         leaderboard.innerText += secondName + " " + second  + "\n";
@@ -42,33 +43,54 @@ function outOfTime() {
 // called when user clicks submit button on end modal
 function addPost(name, score) {
     var dbRef = firebase.database().ref();
-    var top, second, third;
+    var top, second, third, topName, secondName, thirdName;
     // want to add high score here
-    // verify that the button wasn't double clicked
-    // check that name and scores don't match
     dbRef.once('value').then(function(snapshot) {
         top =  snapshot.val().topScore;
         second =  snapshot.val().secondScore;
         third =  snapshot.val().thirdScore;
 
-        if (score > top) {
+        topName =  snapshot.val().topName;
+        secondName =  snapshot.val().secondName;
+        thirdName =  snapshot.val().thirdName;
+
+        if (score > top && !checkAlreadyClicked(name, score, topName, top, secondName, second, thirdName, third)) {
             // move all down
-            console.log("top is", top)
+            console.log("third name is", thirdName, third)
             dbRef.update({ topScore: Number(score) });
+            dbRef.update({ topName: name });
             dbRef.update({ secondScore: top });
+            console.log("third name is", thirdName, third)
+            dbRef.update({ secondName: topName });
+            console.log("third name is", thirdName, third)
             dbRef.update({ thirdScore: second });
+            console.log("third name is", thirdName, third)
+            dbRef.update({ thirdName: secondName });
+            console.log("third name is", thirdName, third)
         }
-        else if (score > second) {
+        else if (score > second && !checkAlreadyClicked(name, score, topName, top, secondName, second, thirdName, third)) {
             // move bottom two down
             dbRef.update({ secondScore: Number(score) });
+            dbRef.update({ secondName: name });
             dbRef.update({ thirdScore: second });
+            dbRef.update({ thirdName: secondName });
         }
-        else if (score > third) {
+        else if (score > third && !checkAlreadyClicked(name, score, topName, top, secondName, second, thirdName, third)) {
             // replace lowest score
+            console.log("third name is", thirdName, third)
             dbRef.update({ thirdScore: Number(score) });
+            dbRef.update({ thirdName: name });
         }
-        
       }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
     });
+}
+
+function checkAlreadyClicked(name, score, topName, topScore, secondName, secondScore, thirdName, thirdScore) {
+    if (name === topName && score === topScore ||
+        name === secondName && score === secondScore ||
+        name === thirdName && score === thirdScore) {
+            return true;
+    }
+    else return false;
 }
