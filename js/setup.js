63e4fcf45bbcad86ab076c10fc50898e2dbf4a07
Sook-Hee <sook-hee.evans@tufts.edu>
2020-06-21 20:50:26 -0400

    bug fix

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,22 @@
 function newSetup() {
     $(".column").css("background-color", "lightyellow");
-    var num_entries = Math.round(Math.random() * 10) - 1;
-    $("#word").html(dict.words[num_entries].word)
-    for (j in dict.words[num_entries].matches) {
-        var word = "#word" + j;
-        $(word).html(dict.words[num_entries].matches[j])
+    var num_entries = Math.round(Math.random() * 34) - 1;
+    var num_matches = dict.words[num_entries].matches.length;
+    var rand_match_1 = Math.round(Math.random() * num_matches) - 1;
+    $("#word").html(dict.words[num_entries].matches[rand_match_1])
+    var num_matches = dict.words[num_entries].matches.length;
+    var rand_match_2 = Math.round(Math.random() * num_matches);
+    var switch_num = Math.round(Math.random() * 1);
+    console.log("sswitch_numis", switch_num);
+    console.log("rsand maatch is, ", rand_match_2)
+    // then randomize length
+    //  then mod by number of words
+
+    switch_num = switch_num *  Math.round(Math.random() * num_matches);
+    switch_num = switch_num % num_matches;
+    rand_match_2 = switch_num;
+    if (rand_match_2 == rand_match_1) {
+        rand_match_2 = (rand_match_2 + 1) % num_matches
     }
+    $("#word0").html(dict.words[num_entries].matches[rand_match_2])
 }
